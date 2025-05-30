10810c4da691b3d8b81d335ae567946aa3938556
tpqms2061 <tpqms2061@naver.com>
2025-05-30 14:35:41 +0900

    varialbes

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int age = 25;
+        System.out.println(age);
+
+       age=26;
+
+        System.out.println(age);
+
+        int salary = (int) 50000.75; //강제 형변환
+        System.out.println(salary);
+
+        char name = 'a';
+        System.out.println(name);
+
+        String name2 = "홍성현";
+        System.out.println(name2);
+
+        System.out.println(name2.length()); //이름의 글자수 호출
     }
 }
