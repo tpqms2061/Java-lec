9953ab59a0304b1a5424036807caada1c9552eff
tpqms2061 <tpqms2061@naver.com>
2025-06-04 17:12:38 +0900

    조건문 - if 문
    - if문
    - if else-if else 문

---
+++ src/three/if1.java
@@ -0,0 +1,14 @@
+package three;
+
+public class if1 {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+//        if(조건문){ 실행문;}
+
+        if (number > 0) {
+            System.out.println("numeber는 0보다 큽니다");
+        }
+    }
+}
---
+++ src/three/if2.java
@@ -0,0 +1,16 @@
+package three;
+
+public class if2 {
+    public static void main(String[] args) {
+
+        int number = 1;
+
+        if (number > 0) {
+            System.out.println("number 는 양수입니다.");
+        } else if (number < 0) {
+            System.out.println("number 는 음수입니다");
+        } else {
+            System.out.println("number 0입니다");
+        }
+    }
+}
