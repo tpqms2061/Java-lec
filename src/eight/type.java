29db178fe368eaa3cf0cbb22592dc10c32c8906c
tpqms2061 <tpqms2061@naver.com>
2025-06-11 23:37:21 +0900

    Printf
    - print의 타입형

---
+++ src/eight/type.java
@@ -0,0 +1,17 @@
+package eight;
+
+public class type {
+
+    public static void main(String[] args) {
+//        System.out.printf("줄바꿈 가능");
+//        System.out.printf("아니오");
+
+        String name = "승환";
+        int age =20;
+        double pi = 3.14159384;
+
+        System.out.printf( "이름: %s\n나이 : %d\n원주율 : %.4f", name, age, pi);
+    }
+
+
+}
