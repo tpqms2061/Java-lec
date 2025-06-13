a25f59d5a9643665e45b11bb1f279af5500526d8
tpqms2061 <tpqms2061@naver.com>
2025-06-13 09:36:54 +0900

    Print
    - Print / Pritfln / Printf Test

---
+++ src/eight/Print.java
@@ -0,0 +1,30 @@
+package eight;
+
+public class Print {
+
+    public static void main(String[] args) {
+//        System.out.print("프린트 함수랑");
+//        System.out.println("프린트 함수 LN함수의");
+//        System.out.print("차이점을 찾아서");
+//        System.out.println("디스코드 채팅으로 ");
+//        System.out.print("알려주시면 ");
+//        System.out.println("될것 같습니다");
+//
+//
+       String name = "Son";
+        int backNumber = 7;
+//
+//
+        System.out.println(name+ backNumber); // 문자열+ 숫자 : 문자열로 인식 _ String
+        System.out.println((name+ backNumber).getClass()); //getClass() 해당 클래스 정보 출력
+       System.out.println((name+ backNumber).getClass().getName()); // Class없이 출력
+
+//        System.out.println("\"안녕하세요\"");
+//        System.out.print("\"안녕하세요?\" \\");
+//        System.out.print("입력하고 싶니?\n 아니오");
+//        System.out.print("입력하고 싶니?\t아니오");
+
+
+
+    }
+}
