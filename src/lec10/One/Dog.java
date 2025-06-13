600fc7f5f0cfb2c04647c2f41e2635e0d80ae8b1
tpqms2061 <tpqms2061@naver.com>
2025-06-13 10:01:10 +0900

    access Modifier
    - Private Test

---
+++ src/lec10/One/Dog.java
@@ -0,0 +1,9 @@
+package lec10.One;
+
+public class Dog extends Animal {
+    public Dog() {
+
+        System.out.println(Animal.PrivateFiled);
+
+    }
+}
---
+++ src/lec10/One/Main.java
@@ -0,0 +1,9 @@
+package lec10.One;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
