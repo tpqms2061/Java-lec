5d3f2b60b5e55f4bf19306c48e0bc79290b617ca
tpqms2061 <tpqms2061@naver.com>
2025-06-17 14:35:07 +0900

    multiply Exception Throws

---
+++ src/lec12/trycatch/Throws3.java
@@ -0,0 +1,30 @@
+package lec12.trycatch;
+
+public class Throws3 {
+
+    public static void main(String[] args) {
+        String test = "2,a43";
+
+        try {
+            int result = multiply(test);
+            System.out.println(result);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            System.out.println("에러 !");
+
+        }
+    }
+
+    public static int multiply(String input) throws NumberFormatException {
+        String[] splitInput = input.split(",");
+        int[] results = new int[2];
+
+        for (int i = 0; i < splitInput.length; i++) {
+            results[i] = Integer.parseInt(splitInput[i]);
+        }
+        return results[0] * results[1];
+    }
+
+
+}
+
