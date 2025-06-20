f2ae57070aa94ce8d881d4ea15a73179111a3890
tpqms2061 <tpqms2061@naver.com>
2025-06-20 22:28:01 +0900

    Synchronized

---
+++ src/lec15/syncronized/SMain1.java
@@ -0,0 +1,40 @@
+package lec15.syncronized;
+
+class Counter {
+    private int count =0;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class SMain1 {
+    public static void main(String[] args) throws InterruptedException{
+
+
+        Counter counter = new Counter();
+
+        Runnable task = ( ) -> {                //쓰레드 로직만 생성
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        };
+
+        Thread[] threads = new Thread[5]; //쓰레드  배열 5개 생성 /안에 배열 x
+        for (int i = 0; i < 5; i++) {
+            threads[i] = new Thread(task); //for문을 통해 안에 내용들 설정
+            threads[i].start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
+        System.out.println("Final count :" + counter.getCount());
+
+    }
+}
