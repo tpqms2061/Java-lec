ca7574f9584b0a7b175e5b594522a8cbdd24b841
tpqms2061 <tpqms2061@naver.com>
2025-06-23 11:26:10 +0900

    Parallel Processing

---
+++ src/lec16/test/test3.java
@@ -0,0 +1,13 @@
+//package lec16.test;
+//
+//public class test3 {
+//
+//    public static int getDivisorSum(int n) {
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(getDivisorSum(12));
+//        System.out.println(getDivisorSum(5));
+//    }
+//}
